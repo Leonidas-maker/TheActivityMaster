bd8f5fe99a0093e72ef8f3b71408af05cda9be7a
Schuetze1000 <schuetzeandreas.1@web.de>
2025-03-14 22:27:53 +0100

    Added specific debug response for user emails verifications; Added start python script to prepare a test environment

--- server/app/controllers/auth.py
+++ server/app/controllers/auth.py
@@ -270,8 +270,8 @@ async def login(
                 ),
             )
 
-        if ENVIRONMENT == "dev":
-            print(f"Email code: {email_code}")
+        if ENVIRONMENT == "dev" and DEBUG:
+            print(f"UserID: {user.id}, Email Code: {email_code}")
 
     security_token = await create_security_token(ep_context, user.id, application_id, ["2fa"], ip_address)
     audit_logger.user_login(user.id, ip_address)
@@ -471,7 +471,7 @@ async def forgot_password(
         )
 
     if ENVIRONMENT == "dev" and DEBUG:
-        print(urlencode({"security_token": security_token}))
+        print(f"UserID: {user.id}, Security Token: {security_token}")
     audit_logger.user_forgot_password(user.id, ip_address)
     await db.commit()
 
